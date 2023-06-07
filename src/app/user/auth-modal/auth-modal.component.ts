849cf433c3b2de37b7ad85fa454cd18668b0341f
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-07 08:42:47 +0530

    creating shared modal

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
-  styleUrls: ['./auth-modal.component.css']
+  styleUrls: ['./auth-modal.component.css'],
 })
-export class AuthModalComponent {
+export class AuthModalComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
