a17281943ef10bae7c3133634e64f7895f575754
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-10 09:51:30 +0530

    adding form controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
