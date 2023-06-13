7ceb5684ea67835eeafffde93b6642aaa4a59602
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-13 17:27:52 +0530

    importing angularfire

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAEVWbwPueASX010WcjDsy83Xy6rqAkJCY',
+    authDomain: 'clips-ab20b.firebaseapp.com',
+    projectId: 'clips-ab20b',
+    storageBucket: 'clips-ab20b.appspot.com',
+    appId: '1:312909105378:web:5dd61d57238f36fab9b6bb',
+  },
+};
