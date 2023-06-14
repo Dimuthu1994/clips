254332b486dc4d8cf1eff02a9a86d92aae634635
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-14 09:19:20 +0530

    setting up the login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,30 @@ export class LoginComponent {
     email: '',
     password: '',
   };
-  login() {
-    console.log(this.credentials);
+  showAlert = false;
+  alertMsg = 'Please wait!';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) {}
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait!';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error) {
+      this.inSubmission = false;
+      this.alertMsg = 'An unexpected error occured.';
+      this.alertColor = 'red';
+      return;
+    }
+    this.alertMsg = 'Success! You are now logged in.';
+    this.alertColor = 'green';
   }
 }
