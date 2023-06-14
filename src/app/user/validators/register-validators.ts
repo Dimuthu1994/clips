94dcf462ce6bd5bcab08bf6457cd6be2abd50923
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-14 17:38:39 +0530

    factory function

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,15 +1,17 @@
-import { ValidationErrors, AbstractControl } from '@angular/forms';
+import { ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
 
 export class RegisterValidators {
-  static match(group: AbstractControl): ValidationErrors | null {
-    const control = group.get('password');
-    const matchingControl = group.get('confirm_password');
+  static match(controlName: string, matchingControlName: string): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+      const control = group.get(controlName);
+      const matchingControl = group.get(matchingControlName);
 
-    if (!control || !matchingControl) {
-      return { controlNotFound: false };
-    }
-    const error =
-      control.value === matchingControl.value ? null : { noMatch: true };
-    return error;
+      if (!control || !matchingControl) {
+        return { controlNotFound: false };
+      }
+      const error =
+        control.value === matchingControl.value ? null : { noMatch: true };
+      return error;
+    };
   }
 }
