87d6c141db1d20de10f892ba5c0b251e64775895
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-15 19:13:51 +0530

    query parameters 2

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -7,18 +7,19 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
-  videoOrder = 1;
+  videoOrder = '1';
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
-      this.videoOrder = params.sort === '2' ? params.sort : '1';
+      //console.log('params.sort', params.get('sort'));
+      this.videoOrder = params.get('sort') === '2' ? params.get('sort') : '1';
     });
   }
 
   sort($event: Event) {
-    const { value } = $event?.target as HTMLSelectElement;
+    const { value } = $event.target as HTMLSelectElement;
 
-    this.router.navigateByUrl('/manage?sort=${value}');
+    this.router.navigateByUrl(`/manage?sort=${value}`);
   }
 }
