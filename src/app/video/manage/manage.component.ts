2bfb61cd2379897a472725d07844b31752f0867e
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-16 17:08:32 +0530

    getting list of clips from db

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
+import IClip from 'src/app/models/clips';
 
 @Component({
   selector: 'app-manage',
@@ -9,6 +10,7 @@ import { ClipService } from 'src/app/services/clip.service';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -20,7 +22,16 @@ export class ManageComponent implements OnInit {
       //console.log('params.sort', params.get('sort'));
       this.videoOrder = params.get('sort') === '2' ? params.get('sort') : '1';
     });
-    this.clipService.getUserClips().subscribe(console.log);
+    this.clipService.getUserClips().subscribe((docs) => {
+      this.clips = [];
+
+      docs.forEach((docs) => {
+        this.clips.push({
+          docID: docs.id,
+          ...docs.data(),
+        });
+      });
+    });
   }
 
   sort($event: Event) {
