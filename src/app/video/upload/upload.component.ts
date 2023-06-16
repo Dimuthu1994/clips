46cb800153775d0d6248190dc33e5920b4fc8164
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-16 16:34:39 +0530

    redirect page after upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -9,6 +9,7 @@ import { last, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -31,7 +32,8 @@ export class UploadComponent implements OnDestroy {
   constructor(
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
-    private clipsService: ClipService
+    private clipsService: ClipService,
+    private router: Router
   ) {
     auth.user.subscribe((user) => (this.user = user));
   }
@@ -88,7 +90,7 @@ export class UploadComponent implements OnDestroy {
         switchMap(() => clipRef.getDownloadURL())
       )
       .subscribe({
-        next: (url) => {
+        next: async (url) => {
           const clip = {
             uid: this.user?.uid as string,
             displayName: this.user?.displayName as string,
@@ -96,12 +98,16 @@ export class UploadComponent implements OnDestroy {
             fileName: `${clipFileName}.mp4`,
             url,
           };
-          this.clipsService.createClip(clip);
+          const clipDocRef = await this.clipsService.createClip(clip);
           console.log(clip);
           this.alertColor = 'green';
           this.alertMsg =
             'Success! Your clip s now ready to share with the world.';
           this.showPercentage = false;
+
+          setTimeout(() => {
+            this.router.navigate(['clip', clipDocRef.id]);
+          }, 1000);
         },
         error: (error) => {
           this.uploadForm.enable();
