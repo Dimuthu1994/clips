7b9f01239e91d31cfd5cde132f912dfa10ab0614
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-21 13:51:49 +0530

    uploading a blob

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -68,4 +68,11 @@ export class FfmpegService {
     this.isRunning = true;
     return screenshots;
   }
+
+  async blobFromURL(url: string) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+
+    return blob;
+  }
 }
